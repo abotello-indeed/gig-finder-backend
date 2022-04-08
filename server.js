40713f41b5d679857086659eff5316f816dc492f
Anthony Botello <abotello@indeed.com>
2022-04-08 02:43:17 -0500

    added responses to 'connect' and 'postGig' actions
    - when client sends 'connect' action the server will query the database for all jobs and send the list to the client
    - when client sends 'postGig' action the server will insert the new gig into the database and emit an update to all connected clients with an updated list of gigs

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const ws = require('ws');
+const db = require('./database/db');
+
 const server = new ws.WebSocketServer({ port: 8080 });
 
-server.on('connection', (socket, req) => {
-    socket.on('message', (data) => {
+server.on('connection', (socket) => {
+    socket.on('message', async (data) => {
         const req = JSON.parse(data);
         const action = req.action;
-        
+
         if(action === "connect"){
-            //TO-DO: retrieve and send jobs from database
+            const response = JSON.stringify(await db.gig.findAll());
+            socket.send(response);
         }
         else if(action === "postGig") {
-            //TO-DO: post new gig to database and broadcast it to all clients
+            const data = req.data;
+            await db.gig.create(data);
+
+            // broadcast updated list of gigs to all clients
+            const response = JSON.stringify({
+                action: "update",
+                data: (await db.gig.findAll())
+            });
+
+            server.clients.forEach((client) => {
+               if(client.readyState === ws.WebSocket.OPEN) {
+                   client.send(response);
+               }
+            });
         }
     });
 });
